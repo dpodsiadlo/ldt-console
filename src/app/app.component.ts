ce5bb6883903bd4dd01030679a4b2ad224710041
Daniel Podsiadło <daniel@hungrymedia.com>
2016-09-06 12:53:24 +0200

    Added filtering by request URL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ import {LogPackage} from "./services/data";
 export class AppComponent {
 
     private port:number = 1800;
+    private filter:string = "";
+    private filterRE:any = null;
     private maxEntries:number = 10;
     private connected:boolean = false;
     private defaultState:string = "minimized";
@@ -30,15 +32,42 @@ export class AppComponent {
         this.logChannel.onMessage.addListener((msg:any) => {
             if (msg.command == 'PrintLog')
                 this.ng.run(()=> {
-                    this.log.unshift(msg.data);
 
-                    if (this.log.length > this.maxEntries) {
-                        this.log.splice(this.maxEntries);
+                    if (this.filter == "" || (this.filterRE !== null && msg.data.request && this.filterRE.test(msg.data.request.url) || (msg.data.request && msg.data.request.url.indexOf(this.filter) >= 0))) {
+                        this.log.unshift(msg.data);
+
+                        if (this.log.length > this.maxEntries) {
+                            this.log.splice(this.maxEntries);
+                        }
                     }
+
+
                 });
         });
 
     }
+
+    public filterChanged(filter:string) {
+
+        if (filter != "") {
+            try {
+                this.filterRE = new RegExp(filter);
+
+            } catch (err) {
+                this.filterRE = null;
+            }
+
+        } else {
+            this.filterRE = null;
+        }
+
+    }
+
+    public isFilterRE():boolean {
+
+        return this.filterRE != null;
+    }
+
 
     public isConnected():boolean {
         return this.connected;
